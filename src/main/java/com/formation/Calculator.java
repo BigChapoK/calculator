89186b9ade167a9f0247871ddac41d95b0ad6ea8
BigChapoK <theo.renoton@free.fr>
2026-06-12 12:47:57 +0000

    correction PMD

--- src/main/java/com/formation/Calculator.java
+++ src/main/java/com/formation/Calculator.java
@@ -20,7 +20,6 @@ public class Calculator {
     }
 
     public int divide(int a, int b) {
-        int result = 0;
         if (b == 0) {
             throw new ArithmeticException("Division par zéro impossible");
         }
